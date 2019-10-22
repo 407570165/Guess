2982d6ae4b8951058158b6b070820afc07242bdb
miia <miia@com.tw>
2019-10-22 16:48:06 +0800

    10/22

---
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -0,0 +1,83 @@
+package com.tom.guess;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    int secret =new Random().nextInt(10)+1;
+    private EditText g;
+    private TextView total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        g = findViewById(R.id.guess);
+        total = findViewById(R.id.result);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void zero(View view){
+        int i=0;
+
+    }
+    public void turn(View view){
+       int guess=Integer.parseInt(g.getText().toString());
+       if (guess>secret){
+           total.setText("SMALLER");
+       }else if (guess<secret){
+           total.setText("BIGGER");
+       }else if (guess==secret){
+            total.setText("BINGO");
+       }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
